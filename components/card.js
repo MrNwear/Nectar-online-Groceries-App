ff6641ab819e0f6f28a2c61cf9153e0afdbcf95e
Yousef Nwear <mr.y.neuer@gmail.com>
2022-04-04 06:09:26 +0200

    Card Component and Shop Screen

--- components/card.js
+++ components/card.js
@@ -1,22 +1,28 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from 'react-native';
-
-export function Card() {
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { MainButton } from "./button";
+export const Card = (props) => {
+    const imagesource = props.source
+    const cardTitle = props.title
     return (
-        <View style={styles.container}>
-            <Image style={{ height: '40%', width: '80%', resizeMode: "contain", margin: 5 }} source={require('../assets/apple.png')} />
-            <Text style={{ fontWeight: '700', color: '#000', margin: 5 }}>Apple</Text>
-            <Text style={{ margin: 5 }}>1kg, 15 L.E</Text>
-
-        </View>
+        <TouchableOpacity style={styles.container}>
+            <Image style={{ height: '40%', width: '80%', resizeMode: "contain", margin: 5 }} source={imagesource} />
+            <Text style={{ fontWeight: '700', color: '#000', margin: 5 }}>{cardTitle}</Text>
+            <View style={{
+                justifyContent: "space-between", flexDirection: "row"
+            }}>
+                <Text style={{ margin: 5 }}>1kg, 15 L.E</Text>
+                <MainButton title='+' style={{ width: '25%', backgroundColor: '#53B175', borderRadius: 10, marginRight: 5 }} txtstyle={{ color: '#fff' }} />
+            </View>
+        </TouchableOpacity>
     )
 }
 const styles = StyleSheet.create({
     container: {
-        width: '40%',
-        height: '30%',
+        flex: 1,
         borderRadius: 25,
         borderWidth: 1,
-        margin: 5,
+        width: 150,
+        height: 180, margin: 10
     }
 })
--- screens/shop.js
+++ screens/shop.js
@@ -1,13 +1,65 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-
+import { Input } from 'react-native-elements';
+import { Card } from '../components/card';
 export const ShopScreen = () => {
     return (
-        <View>
+        <ScrollView style={styles.container} contentContainerStyle={{ alignItems: 'center' }} >
+            <Image style={styles.imagestyle} source={require('../assets/orangeCarrot.png')} />
             <Text>
-                <Icon name="rocket" size={30} color="#900" />
+                <Icon name="map-marker" size={15} color="#000" /> Cairo , Egypt
             </Text>
-        </View>
+            <Input
+                placeholder='Search'
+                leftIcon={
+                    <Icon
+                        name='search'
+                        size={20}
+                        color='black'
+                    />
+                }
+                autoFocus={false}
+
+                inputContainerStyle={{ height: 30, width: '90%', borderRadius: 10, borderWidth: 1, paddingLeft: 5, marginTop: 10, alignSelf: 'center' }}
+            />
+            <Image source={require('../assets/freshVegetables.png')} style={{ width: '90%', resizeMode: 'contain' }} />
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: '100%' }}>
+                <Text style={{ fontWeight: '700', color: '#000', marginLeft: 10 }}>Exclusive Offer</Text>
+                <TouchableOpacity style={{ width: '30%', marginRight: 5 }}><Text style={{ color: '#53B175' }}>Resend Code</Text></TouchableOpacity>
+            </View>
+            <ScrollView horizontal nestedScrollEnabled style={{ height: 500, flex: 1 }}>
+
+                <Card title='Apple' source={require('../assets/apple.png')} />
+                <Card title='Carrot' source={require('../assets/orangeCarrot.png')} />
+                <Card title='Apple' source={require('../assets/apple.png')} />
+                <Card title='Carrot' source={require('../assets/orangeCarrot.png')} />
+                <Card title='Apple' source={require('../assets/apple.png')} />
+                <Card title='Carrot' source={require('../assets/orangeCarrot.png')} />
+                <Card title='Apple' source={require('../assets/apple.png')} />
+                <Card title='Carrot' source={require('../assets/orangeCarrot.png')} />
+            </ScrollView>
+        </ScrollView>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+
+
+    },
+    imagestyle: {
+        marginTop: 20,
+        height: 30,
+        width: 30,
+        resizeMode: 'contain',
+
+    },
+    cardcontainer: {
+        width: '40%',
+        height: '30%',
+        borderRadius: 25,
+        borderWidth: 1,
+        margin: 5,
+    }
+})
